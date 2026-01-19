c0266f09505be5768ca701782a6a04142326519a
Anton Lidström <antonlidstroem@outlook.com>
2026-01-19 15:44:09 +0100

    checked if app i startable

---
+++ BudgetPlanner8.DAL/Migrations/20260119144008_updateTransactionModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetPlanner8.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTransactionModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Month",
+                table: "Transactions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Month",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
